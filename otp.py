0d4b45b0c40ef4c5fa7577e3cc386ee9364c6bec
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-01-23 18:53:07 +0530

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,50 +1,42 @@
-from flask import Flask, render_template, request, jsonify
-import smtplib
-import random
-from email.message import EmailMessage
+from flask import *
+from flask_mail import *
+from random import *
 
 app = Flask(__name__)
+#mail = Mail(app)
 
-# Generate OTP
-def generate_otp():
-    otp = ''.join([str(random.randint(0, 9)) for _ in range(6)])
-    return otp
 
-# Email sending function
-def send_email(to_email, otp):
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        from_mail = 'family005778@gmail.com'
-        server.login(from_mail, 'bwde dnao axmg wdxz')
+app.config["MAIL_SERVER"]='smtp.gmail.com'
+app.config["MAIL_PORT"] = '587'
+app.config["MAIL_USERNAME"] = 'family005778@gmail.com'
+app.config["MAIL_PASSWORD"]= 'btyx smvk ubdl mkdc'
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 
-        msg = EmailMessage()
-        msg['Subject'] = "OTP Verification"
-        msg['From'] = from_mail
-        msg['To'] = to_email
-        msg.set_content(f"Your OTP is: {otp}")
 
-        server.send_message(msg)
-        server.quit()
-        return True
-    except Exception as e:
-        print(f"Error: {e}")
-        return False
+mail = Mail(app)
+otp = randint(000000,999999)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('verification.html')
+    return render_template("verify_otp.html")
 
-@app.route('/send_otp', methods=['POST'])
-def send_otp():
-    data = request.json
-    to_email = data.get('email')
-    otp = generate_otp()
+@app.route('/verify',methods = ["POST"])
+def verify():
+    email = request.form["email"]
 
-    if send_email(to_email, otp):
-        return jsonify({"status": "success", "message": "OTP sent successfully!", "otp": otp})
-    else:
-        return jsonify({"status": "error", "message": "Failed to send OTP."})
+    msg = Message('OTP',sender = 'family005778@gmail.com', recipients = [email])
+    msg.body = str(otp)
+    mail.send(msg)
+    return render_template('verify.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/validate',methods = ["POST"])
+def validate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp):
+        return "<h3>Email verified successfully</h3>"
+    return "<h3>failure</h3>"
+    
+    
+if __name__ == "__main__":
+    app.run(debug = True)
